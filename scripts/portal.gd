d4a3082ad3a441cb28567936dd8eaadd6848b3fc
JFVIANA10 <jfvyana@gmail.com>
2025-12-05 11:43:48 -0300

    add portal

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
-@export var nome_porta_saida: String
-@export var nome_porta_entrada: String
+@export var destino: String
+@export var origem: String
+@export var distancia: int
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		var portais = get_tree().get_nodes_in_group("portais")
-		var proxima_posicao = position
+	
 		
 		for portal in portais:
-			if portal.get("nome_porta"):
-				if portal.nome_porta == nome_porta_saida:
-					proxima_posicao = portal.postion
+			if portal.get("origem"):
+				if portal.origem == destino:
+					var x = portal.position.x + portal.distancia
 					
-		body.position = Vector2 (proxima_posicao.y, proxima_posicao.x + 20)
+					var y = portal.position.y 
+					if Input.is_action_just_pressed("entrar"):
+						body.position = Vector2 (x,y)
+		
